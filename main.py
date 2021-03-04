862f5c472d0d9e606a8d7db4700c6cbd002d7c9b
Bodeu97 <lukaszebo@gmail.com>
2021-03-04 14:10:58 +0100

    zad 1 i 2

---
+++ main.py
@@ -0,0 +1,23 @@
+
+# # Zadanie 1
+# # int
+# a, b=1, 2
+# # float
+# c, d=1.1, 2.2
+# # complex
+# e, f=1+1j, 2+2j
+# # string
+# str1, str2="string1", "string2"
+#
+# print(a, b, c, d, e, f, str1, str2)
+
+
+# Zadanie 2
+a = 1
+b = 2
+dodawanie = a+b
+odejmowanie = a-b
+dzielenie = a/b
+mnozenie = a*b
+potegowanie = a**b
+pierwiastkowanie = a**(1/b)
